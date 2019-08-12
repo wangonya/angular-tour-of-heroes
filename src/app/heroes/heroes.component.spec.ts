76cf9b65c2513fa408684a070b34419484fa0746
wangonya <kwangonya@gmail.com>
2019-08-12 13:22:32 +0300

    improve ui and add heroes component tests

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,13 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { HeroesComponent } from './heroes.component';
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let fixture: ComponentFixture<HeroesComponent>;
+  let compiled;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [ HeroesComponent ]
     })
     .compileComponents();
@@ -16,10 +19,11 @@ describe('HeroesComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeroesComponent);
     component = fixture.componentInstance;
+    compiled = fixture.debugElement.nativeElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render title in a h3 tag', () => {
+    expect(compiled.querySelector('h3').textContent).toContain(`${component.hero.name.toUpperCase()} Details`);
   });
 });
